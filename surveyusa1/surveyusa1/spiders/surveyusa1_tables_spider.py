14b7223ab6fd02413f64185f12968a3ab4fa0e8a
Simran Purewal <spure001@wch135-37.cs.ucr.edu>
2020-02-16 16:34:00 -0800

    python script somewhat works

--- surveyusa1/surveyusa1/spiders/surveyusa1_tables_spider.py
+++ surveyusa1/surveyusa1/spiders/surveyusa1_tables_spider.py
@@ -9,18 +9,18 @@ class SurveyUSA1TablesSpider(scrapy.Spider):
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
 
-		def parse(self, response):
-			for row in response.xpath('//*[@class="qtable"]//tr'):
-				yield {
-					'r1' : row.xpath('td[1]//text()').extract_first(),
-					'r2' : row.xpath('td[2]//text()').extract_first(),
-					'r3' : row.xpath('td[3]//text()').extract_first(),
-					'r4' : row.xpath('td[4]//text()').extract_first(),
-					'r5' : row.xpath('td[5]//text()').extract_first(),
-					'r6' : row.xpath('td[6]//text()').extract_first(),
-					'r7' : row.xpath('td[7]//text()').extract_first(),
-					'r8' : row.xpath('td[8]//text()').extract_first(),
-					'r9' : row.xpath('td[9]//text()').extract_first(),
-					'r10' : row.xpath('td[10]//text()').extract_first(),
-					'r11' : row.xpath('td[11]//text()').extract_first(),
-				}
+	def parse(self, response):
+		for row in response.xpath('//*[@class="qtable"]//tr'):
+			yield {
+				'r1' : row.xpath('td[1]//text()').extract_first(),
+				'r2' : row.xpath('td[2]//text()').extract_first(),
+				'r3' : row.xpath('td[3]//text()').extract_first(),
+				'r4' : row.xpath('td[4]//text()').extract_first(),
+				'r5' : row.xpath('td[5]//text()').extract_first(),
+				'r6' : row.xpath('td[6]//text()').extract_first(),
+				'r7' : row.xpath('td[7]//text()').extract_first(),
+				'r8' : row.xpath('td[8]//text()').extract_first(),
+				'r9' : row.xpath('td[9]//text()').extract_first(),
+				'r10' : row.xpath('td[10]//text()').extract_first(),
+				'r11' : row.xpath('td[11]//text()').extract_first(),
+			}
